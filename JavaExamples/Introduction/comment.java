df5d860935a49a941d7a04a489e6d2cb01cbe8a1
Anubhav Vardhan <vardhananubhav@gmail.com>
2021-06-05 14:47:42 +0530

    comment

---
+++ JavaExamples/Introduction/comment.java
@@ -0,0 +1,27 @@
+package JavaExamples.Introduction;
+
+public class comment {
+    public static void main(String[] args) {
+        
+        //SINGLE LINE COMMENT
+        // compiler ignores everything from // to end of line. hence also called end of line comment
+
+        /*MULTI
+        *LINE
+        *COMMENT
+        */
+        
+        //also called traditional comment
+
+
+        //GOOD HABITS
+        
+        /*comments shouldn't be the substitute for a way to explain poorly written code in English
+         *always write well structured and self explaining code. And, then use comments.
+         *We can use comments to explain complex algorithms, regex or scenarios where we have to choose one technique among different technique to solve problems.
+         *
+         * In most cases, always use comments to explain 'why' rather than 'how' and you are good to go. 
+         */
+
+    }
+}
